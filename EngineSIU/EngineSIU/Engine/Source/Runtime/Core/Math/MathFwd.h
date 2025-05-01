408c4485948ea5a84b7b96a23701e9aca0582d69
gudtldn <gudtldn1234@gmail.com>
2025-05-01 18:59:48 +0900

    feat: MathFwd.h 파일 추가 및 vcxproj에 포함

---
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Core/Math/MathFwd.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+// ReSharper disable once CppUnusedIncludeDirective
+#include "HAL/PlatformType.h"
+
+struct FColor;
+struct FLinearColor;
+
+struct FVector;
+struct FVector2D;
+struct FVector4;
+struct FQuat;
+struct FMatrix;
+struct FPlane;
+
+struct FRotator;
