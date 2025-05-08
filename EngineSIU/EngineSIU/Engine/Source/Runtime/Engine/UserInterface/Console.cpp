06b8e542fbeeeb1baa03ad542b7099d9d04dce2e
gudtldn <gudtldn1234@gmail.com>
2025-05-08 17:29:55 +0900

    fix: 콘솔 로그 추가 시 스크롤을 하단으로 자동 이동하도록 수정

--- EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/UserInterface/Console.cpp
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/UserInterface/Console.cpp
@@ -189,6 +189,8 @@ void FConsole::AddLog(ELogLevel Level, const ANSICHAR* Fmt, ...)
 
     Items.Emplace(Level, std::string(Buf));
     va_end(Args);
+
+    ScrollToBottom = true;
 }
 
 void FConsole::AddLog(ELogLevel Level, const WIDECHAR* Fmt, ...)
@@ -202,11 +204,14 @@ void FConsole::AddLog(ELogLevel Level, const WIDECHAR* Fmt, ...)
 
     Items.Emplace(Level, FString(Buf).ToAnsiString());
     va_end(Args);
+
+    ScrollToBottom = true;
 }
 
 void FConsole::AddLog(ELogLevel Level, const FString& Message)
 {
     Items.Emplace(Level, Message);
+    ScrollToBottom = true;
 }
 
 // 콘솔 창 렌더링
