e69b96bd56d91bb833244592a44e75cfb61dd579
jungjihyung <wlgud3936@gmail.com>
2025-05-09 18:48:15 +0900

    feat: Skeletal Mesh AABB, Ray Picking StaticMesh랑 동일하게 추가

--- EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Components/SkeletalMeshComponent.cpp
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Components/SkeletalMeshComponent.cpp
@@ -74,7 +74,8 @@ void USkeletalMeshComponent::TickComponent(float DeltaTime)
 void USkeletalMeshComponent::SetSkeletalMeshAsset(USkeletalMesh* InSkeletalMeshAsset)
 {
     SkeletalMeshAsset = InSkeletalMeshAsset;
-
+    AABB = FBoundingBox(SkeletalMeshAsset->GetRenderData()->BoundingBoxMin, SkeletalMeshAsset->GetRenderData()->BoundingBoxMax);
+    
     BoneTransforms.Empty();
     BoneBindPoseTransforms.Empty();
     
@@ -127,4 +128,62 @@ void USkeletalMeshComponent::SetAnimationEnabled(bool bEnable)
             BoneTransforms = RefSkeleton.RawRefBonePose;
         }
     }
+}
+
+int USkeletalMeshComponent::CheckRayIntersection(const FVector& InRayOrigin, const FVector& InRayDirection, float& OutHitDistance) const
+{
+    if (!AABB.Intersect(InRayOrigin, InRayDirection, OutHitDistance))
+    {
+        return 0;
+    }
+    if (SkeletalMeshAsset == nullptr)
+    {
+        return 0;
+    }
+    
+    OutHitDistance = FLT_MAX;
+    
+    int IntersectionNum = 0;
+
+    const FSkeletalMeshRenderData* RenderData = SkeletalMeshAsset->GetRenderData();
+
+    const TArray<FSkeletalMeshVertex>& Vertices = RenderData->Vertices;
+    const int32 VertexNum = Vertices.Num();
+    if (VertexNum == 0)
+    {
+        return 0;
+    }
+    
+    const TArray<UINT>& Indices = RenderData->Indices;
+    const int32 IndexNum = Indices.Num();
+    const bool bHasIndices = (IndexNum > 0);
+    
+    int32 TriangleNum = bHasIndices ? (IndexNum / 3) : (VertexNum / 3);
+    for (int32 i = 0; i < TriangleNum; i++)
+    {
+        int32 Idx0 = i * 3;
+        int32 Idx1 = i * 3 + 1;
+        int32 Idx2 = i * 3 + 2;
+        
+        if (bHasIndices)
+        {
+            Idx0 = Indices[Idx0];
+            Idx1 = Indices[Idx1];
+            Idx2 = Indices[Idx2];
+        }
+
+        // 각 삼각형의 버텍스 위치를 FVector로 불러옵니다.
+        FVector v0 = FVector(Vertices[Idx0].X, Vertices[Idx0].Y, Vertices[Idx0].Z);
+        FVector v1 = FVector(Vertices[Idx1].X, Vertices[Idx1].Y, Vertices[Idx1].Z);
+        FVector v2 = FVector(Vertices[Idx2].X, Vertices[Idx2].Y, Vertices[Idx2].Z);
+
+        float HitDistance = FLT_MAX;
+        if (IntersectRayTriangle(InRayOrigin, InRayDirection, v0, v1, v2, HitDistance))
+        {
+            OutHitDistance = FMath::Min(HitDistance, OutHitDistance);
+            IntersectionNum++;
+        }
+
+    }
+    return IntersectionNum;
 }
