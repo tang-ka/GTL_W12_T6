56126cf53c24a1c80c8d6e03ca0cf2b5618f5eaf
gudtldn <gudtldn1234@gmail.com>
2025-05-10 03:09:29 +0900

    refactor: GetPropertyType 함수 로직 개선 및 포인터 처리 로직 추가, uint32 캐스팅 방식 수정

--- EngineSIU/EngineSIU/Engine/Source/Runtime/CoreUObject/UObject/PropertyTypes.h
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/CoreUObject/UObject/PropertyTypes.h
@@ -1,10 +1,16 @@
 ﻿#pragma once
+#include <concepts>
+#include "Container/Array.h"
+#include "Container/Map.h"
+#include "Container/Set.h"
+#include "Templates/IsArray.h"
 #include "magic_enum/magic_enum.hpp"
 
 
 enum class EPropertyType : uint8
 {
     Unknown,                       // 알 수 없는 타입
+    UnresolvedPointer,             // 컴파일 타임에 알 수 없는 포인터 타입
     Int8, Int16, Int32, Int64,     // 부호 있는 정수타입
     UInt8, UInt16, UInt32, UInt64, // 부호 없는 정수타입
     Float, Double,                 // 실수 타입
@@ -22,45 +28,76 @@ enum class EPropertyType : uint8
 template <typename T>
 consteval EPropertyType GetPropertyType()
 {
-    if constexpr (std::is_same_v<T, int8_t>)
-        return EPropertyType::Int8;
-    else if constexpr (std::is_same_v<T, int16_t>)
-        return EPropertyType::Int16;
-    else if constexpr (std::is_same_v<T, int32_t>)
-        return EPropertyType::Int32;
-    else if constexpr (std::is_same_v<T, int64_t>)
-        return EPropertyType::Int64;
-    else if constexpr (std::is_same_v<T, uint8_t>)
-        return EPropertyType::UInt8;
-    else if constexpr (std::is_same_v<T, uint16_t>)
-        return EPropertyType::UInt16;
-    else if constexpr (std::is_same_v<T, uint32_t>)
-        return EPropertyType::UInt32;
-    else if constexpr (std::is_same_v<T, uint64_t>)
-        return EPropertyType::UInt64;
-    else if constexpr (std::is_same_v<T, float>)
-        return EPropertyType::Float;
-    else if constexpr (std::is_same_v<T, double>)
-        return EPropertyType::Double;
-    else if constexpr (std::is_same_v<T, bool>)
-        return EPropertyType::Bool;
-    else if constexpr (std::is_same_v<T, FString>)
-        return EPropertyType::String;
-    else if constexpr (std::is_same_v<T, FName>)
-        return EPropertyType::Name;
-    // else if constexpr (TIsArray<T>::Value)
-    //     return EPropertyType::Array;
-    // else if constexpr (TIsMap<T>::Value)
-    //     return EPropertyType::Map;
-    // else if constexpr (TIsSet<T>::Value)
-    //     return EPropertyType::Set;
-    else if constexpr (std::is_enum_v<T>)
-        return EPropertyType::Enum;
-    else if constexpr (std::is_base_of_v<UObject, T>)
-        return EPropertyType::Object;
-    else if constexpr (std::is_class_v<T>)
+    // 기본 내장 타입들
+    if constexpr (std::same_as<T, int8>)          { return EPropertyType::Int8;   }
+    else if constexpr (std::same_as<T, int16>)    { return EPropertyType::Int16;  }
+    else if constexpr (std::same_as<T, int32>)    { return EPropertyType::Int32;  }
+    else if constexpr (std::same_as<T, int64>)    { return EPropertyType::Int64;  }
+    else if constexpr (std::same_as<T, uint8>)    { return EPropertyType::UInt8;  }
+    else if constexpr (std::same_as<T, uint16>)   { return EPropertyType::UInt16; }
+    else if constexpr (std::same_as<T, uint32>)   { return EPropertyType::UInt32; }
+    else if constexpr (std::same_as<T, uint64>)   { return EPropertyType::UInt64; }
+    else if constexpr (std::same_as<T, float>)    { return EPropertyType::Float;  }
+    else if constexpr (std::same_as<T, double>)   { return EPropertyType::Double; }
+    else if constexpr (std::same_as<T, bool>)     { return EPropertyType::Bool;   }
+
+    // 엔진 기본 타입들
+    else if constexpr (std::same_as<T, FString>)  { return EPropertyType::String; }
+    else if constexpr (std::same_as<T, FName>)    { return EPropertyType::Name;   }
+
+    // 포인터 타입
+    else if constexpr (std::is_pointer_v<T>)
+    {
+        // using PointedToType = std::remove_cv_t<std::remove_pointer_t<T>>;
+        //
+        // // PointedToType이 완전한 타입일 때만 true를 반환.
+        // // 전방 선언 시 false가 될 수 있음.
+        // if constexpr (std::is_base_of_v<UObject, PointedToType>)
+        // {
+        //     return EPropertyType::Object;
+        // }
+
+        // T가 완전한 타입일 때만 true를 반환.
+        // 전방 선언 시 false가 될 수 있음.
+        if constexpr (std::derived_from<T, UObject>)
+        {
+            return EPropertyType::Object;
+        }
+
+        // 전방 선언된 타입이 들어올 경우, 상속관계를 확인할 수 없음
+        // 이때는 UObject일 확률도 있기 때문에 런타임에 검사
+        return EPropertyType::UnresolvedPointer;
+    }
+
+    // 엔진 기본 컨테이너 타입들
+    else if constexpr (TIsTArray<T> || TIsArray<T>) { return EPropertyType::Array;  }
+    else if constexpr (TIsTMap<T>)                  { return EPropertyType::Map;    }
+    else if constexpr (TIsTSet<T>)                  { return EPropertyType::Set;    }
+
+    // enum class만 지원
+    else if constexpr (std::is_scoped_enum_v<T>)    { return EPropertyType::Enum;   }
+
+    // 리플렉션 시스템에 등록된 커스텀 구조체 (StructTraits 또는 유사한 메커니즘 사용)
+    // 이때 T가 UObject 값 타입이 아니어야 함 (UObject 값 타입은 보통 프로퍼티로 사용 안 함)
+    // else if constexpr (TStructTraits<T>::bIsReflectedStruct && !std::is_base_of_v<UObject, T>)
+    // {
+    //     if (OutUnresolvedTypeName) { *OutUnresolvedTypeName = GetTypeNameString<T>(); } // 경우에 따라 이름 필요
+    //     return EPropertyType::Struct; // 또는 StructTraits에 UnresolvedStruct 플래그가 있다면 그것 사용
+    // }
+
+    // 커스텀 구조체
+    else if constexpr (std::is_class_v<T> && !std::derived_from<T, UObject>)
+    {
+        // TODO: 임시용
+        // 나중에 커스텀 구조체 타입에 대해서 만들어야함
         return EPropertyType::Struct;
-    return EPropertyType::Unknown;
+    }
+
+    else
+    {
+        static_assert(!std::same_as<T, T>, "GetPropertyType: Type T is not supported.");
+        return EPropertyType::Unknown;
+    }
 }
 
 enum class EPropertyFlags : uint32  // NOLINT(performance-enum-size)
@@ -83,7 +120,7 @@ struct magic_enum::customize::enum_range<EPropertyFlags>
 // 비트 플래그 연산을 위한 헬퍼 함수들
 inline EPropertyFlags operator|(EPropertyFlags Lhs, EPropertyFlags Rhs)
 {
-    return static_cast<EPropertyFlags>(static_cast<uint32_t>(Lhs) | static_cast<uint32_t>(Rhs));
+    return static_cast<EPropertyFlags>(static_cast<uint32>(Lhs) | static_cast<uint32>(Rhs));
 }
 
 inline EPropertyFlags& operator|=(EPropertyFlags& Lhs, EPropertyFlags Rhs)
@@ -94,16 +131,16 @@ inline EPropertyFlags& operator|=(EPropertyFlags& Lhs, EPropertyFlags Rhs)
 
 inline bool HasFlag(EPropertyFlags Flags, EPropertyFlags FlagToCheck)
 {
-    return (static_cast<uint32_t>(Flags) & static_cast<uint32_t>(FlagToCheck)) != 0;
+    return (static_cast<uint32>(Flags) & static_cast<uint32>(FlagToCheck)) != 0;
 }
 
 // 특정 플래그만 제외하는 연산자
 inline EPropertyFlags operator~(EPropertyFlags Flags)
 {
-    return static_cast<EPropertyFlags>(~static_cast<uint32_t>(Flags));
+    return static_cast<EPropertyFlags>(~static_cast<uint32>(Flags));
 }
 
 inline EPropertyFlags operator&(EPropertyFlags Lhs, EPropertyFlags Rhs)
 {
-    return static_cast<EPropertyFlags>(static_cast<uint32_t>(Lhs) & static_cast<uint32_t>(Rhs));
+    return static_cast<EPropertyFlags>(static_cast<uint32>(Lhs) & static_cast<uint32>(Rhs));
 }
