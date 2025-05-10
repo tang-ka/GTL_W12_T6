97070bad4a2595714b1e65f1dd080b4f9f8d6ea1
gudtldn <gudtldn1234@gmail.com>
2025-05-10 17:35:06 +0900

    feat: TemplateUtilities.h 파일 추가 및 프로젝트 파일에 포함

---
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Core/Templates/TemplateUtilities.h
@@ -0,0 +1,17 @@
+﻿// ReSharper disable CppClangTidyCppcoreguidelinesMissingStdForward
+// ReSharper disable CppCStyleCast
+#pragma once
+#include "HAL/PlatformType.h"
+
+template <typename T>
+FORCEINLINE constexpr std::remove_reference_t<T>&& MoveTemp(T&& Obj) noexcept
+{
+    using CastType = std::remove_reference_t<T>;
+
+    // rvalue나 const 객체가 전달되지 않았는지 확인합니다.
+    // rvalue는 불필요하고, const 객체는 거의 확실히 실수일 가능성이 있습니다.
+    static_assert(std::is_lvalue_reference_v<T>, "MoveTemp called on an rvalue");
+    static_assert(!std::same_as<CastType&, const CastType&>, "MoveTemp called on a const object");
+
+    return (CastType&&)Obj;
+}
