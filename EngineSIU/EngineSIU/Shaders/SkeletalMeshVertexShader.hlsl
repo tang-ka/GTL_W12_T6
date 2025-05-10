417e61c3f9c98e6f689234409f76eadc1c909cfb
Mstone8370 <mstone8370@gmail.com>
2025-05-10 22:25:22 +0900

    fix: 스켈레탈 메시의 탄젠트 문제 수정

--- EngineSIU/EngineSIU/Shaders/SkeletalMeshVertexShader.hlsl
+++ EngineSIU/EngineSIU/Shaders/SkeletalMeshVertexShader.hlsl
@@ -10,7 +10,8 @@ PS_INPUT_SkeletalMesh mainVS(VS_INPUT_SkeletalMesh Input)
     // 스키닝 처리
     float4 SkinnedPosition = float4(0, 0, 0, 0);
     float3 SkinnedNormal = float3(0, 0, 0);
-    
+    float3 SkinnedTangent = float3(0, 0, 0);
+
     // 가중치 합산
     float TotalWeight = 0.0f;
     
@@ -27,9 +28,11 @@ PS_INPUT_SkeletalMesh mainVS(VS_INPUT_SkeletalMesh Input)
             // FBX SDK에서 가져온 역바인드 포즈 행렬이 이미 포함됨
             float4 pos = mul(float4(Input.Position, 1.0f), BoneMatrices[BoneIdx]);
             float3 norm = mul(float4(Input.Normal, 0.0f), BoneMatrices[BoneIdx]).xyz;
+            float3 tan = mul(float4(Input.Tangent.xyz, 0.0f), BoneMatrices[BoneIdx]).xyz;
             
             SkinnedPosition += Weight * pos;
             SkinnedNormal += Weight * norm;
+            SkinnedTangent += Weight * tan;
         }
     }
     
@@ -38,12 +41,14 @@ PS_INPUT_SkeletalMesh mainVS(VS_INPUT_SkeletalMesh Input)
     {
         SkinnedPosition = float4(Input.Position, 1.0f);
         SkinnedNormal = Input.Normal;
+        SkinnedTangent = Input.Tangent.xyz;
     }
     else if (abs(TotalWeight - 1.0f) > 0.001f && TotalWeight > 0.001f)
     {
         // 가중치 합이 1이 아닌 경우 정규화
         SkinnedPosition /= TotalWeight;
         SkinnedNormal /= TotalWeight;
+        SkinnedTangent /= TotalWeight;
     }
     
     Output.Position = SkinnedPosition;
@@ -53,11 +58,10 @@ PS_INPUT_SkeletalMesh mainVS(VS_INPUT_SkeletalMesh Input)
     Output.Position = mul(Output.Position, ViewMatrix);
     Output.Position = mul(Output.Position, ProjectionMatrix);
     
-    Output.WorldNormal = mul(normalize(SkinnedNormal), (float3x3)InverseTransposedWorld);
+    Output.WorldNormal = normalize(mul(normalize(SkinnedNormal), (float3x3)InverseTransposedWorld));
 
     // Begin Tangent
-    float3 WorldTangent = mul(Input.Tangent.xyz, (float3x3)WorldMatrix);
-    WorldTangent = normalize(WorldTangent);
+    float3 WorldTangent = mul(normalize(SkinnedTangent), (float3x3)WorldMatrix);
     WorldTangent = normalize(WorldTangent - Output.WorldNormal * dot(Output.WorldNormal, WorldTangent));
 
     Output.WorldTangent = float4(WorldTangent, Input.Tangent.w);
