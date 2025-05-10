894a4eb3f24e5c316eb1d8f2206cf87247edc1a4
gudtldn <gudtldn1234@gmail.com>
2025-05-10 17:50:40 +0900

    feat: TemplateUtilities.h에 객체 복사 및 전달 관련 함수 추가

--- EngineSIU/EngineSIU/Engine/Source/Runtime/Core/Templates/TemplateUtilities.h
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Core/Templates/TemplateUtilities.h
@@ -1,8 +1,16 @@
 ﻿// ReSharper disable CppClangTidyCppcoreguidelinesMissingStdForward
 // ReSharper disable CppCStyleCast
+// ReSharper disable CppClangTidyCppcoreguidelinesRvalueReferenceParamNotMoved
 #pragma once
 #include "HAL/PlatformType.h"
 
+
+/**
+ * 객체를 우측값으로 캐스팅하여 이동 연산을 수행합니다.
+ * rvalue나 const 객체에 대해서는 사용할 수 없도록 제한됩니다.
+ * @param Obj 이동시킬 객체
+ * @return 이동된 객체의 우측값 참조
+ */
 template <typename T>
 FORCEINLINE constexpr std::remove_reference_t<T>&& MoveTemp(T&& Obj) noexcept
 {
@@ -14,4 +22,61 @@ FORCEINLINE constexpr std::remove_reference_t<T>&& MoveTemp(T&& Obj) noexcept
     static_assert(!std::same_as<CastType&, const CastType&>, "MoveTemp called on a const object");
 
     return (CastType&&)Obj;
+}
+
+/**
+ * 비상수 객체를 복사합니다.
+ * @param Val 복사할 객체
+ * @return 복사된 객체
+ */
+template <typename T>
+FORCEINLINE T CopyTemp(T& Val)
+{
+    return const_cast<const T&>(Val);
+}
+
+/**
+ * 상수 객체를 복사합니다.
+ * @param Val 복사할 객체
+ * @return 복사된 객체
+ */
+template <typename T>
+FORCEINLINE T CopyTemp(const T& Val)
+{
+    return Val;
+}
+
+/**
+ * 좌측값 참조를 완벽 전달합니다.
+ * @param Obj 전달할 객체
+ * @return 전달된 객체의 참조
+ */
+template <typename T>
+FORCEINLINE constexpr T&& Forward(std::remove_reference_t<T>& Obj) noexcept
+{
+    return (T&&)Obj;
+}
+
+/**
+ * 우측값 참조를 완벽 전달합니다.
+ * @param Obj 전달할 객체
+ * @return 전달된 객체의 참조
+ */
+template <typename T>
+FORCEINLINE constexpr T&& Forward(std::remove_reference_t<T>&& Obj) noexcept
+{
+    return (T&&)Obj;
+}
+
+/**
+ * 두 객체의 값을 교환합니다.
+ * @param A 첫 번째 객체
+ * @param B 두 번째 객체
+ */
+template <typename T>
+inline void Swap(T& A, T& B)
+{
+    T Temp = MoveTemp(A);
+    A = MoveTemp(B);
+    B = MoveTemp(Temp);
 }
