3ece22df7707f0414929aad6d2691c91ce654858
gudtldn <gudtldn1234@gmail.com>
2025-05-10 16:51:31 +0900

    feat: FProperty 클래스 확장 및 새로운 멤버 함수와 데이터 추가

--- EngineSIU/EngineSIU/Engine/Source/Runtime/CoreUObject/UObject/Property.cpp
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/CoreUObject/UObject/Property.cpp
@@ -1,2 +1,12 @@
 ﻿#include "Property.h"
 
+
+void FProperty::DisplayInImGui(UObject* ObjectPtr) const
+{
+    // TODO: FProperty를 상속받은 클래스 만들기
+}
+
+void FProperty::Resolve()
+{
+    // TODO: 커스텀 구조체도 가능하게 만들기
+}
--- EngineSIU/EngineSIU/Engine/Source/Runtime/CoreUObject/UObject/Property.h
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/CoreUObject/UObject/Property.h
@@ -1,20 +1,119 @@
 ﻿#pragma once
+#include <variant>
+#include <optional>
+
 #include "Object.h"
+#include "PropertyTypes.h"
 
 
 struct FProperty
 {
-    FProperty(const char* InName, int32 InSize, int32 InOffset)
-        : Name(InName)
+    /**
+     * 클래스의 프로퍼티를 등록합니다.
+     * @param InOwnerClass 이 프로퍼티를 가지고 있는 UClass
+     * @param InPropertyName 프로퍼티의 이름
+     * @param InType 프로퍼티의 타입
+     * @param InSize 프로퍼티의 크기
+     * @param InOffset 프로퍼티가 클래스로부터 떨어진 거리
+     * @param InFlags Reflection 관련 Flag
+     */
+    FProperty(
+        UClass* InOwnerClass,
+        const char* InPropertyName,
+        EPropertyType InType,
+        int32 InSize,
+        int32 InOffset,
+        EPropertyFlags InFlags = EPropertyFlags::None
+    )
+        : OwnerClass(InOwnerClass)
+        , Name(InPropertyName)
+        , Type(InType)
         , Size(InSize)
         , Offset(InOffset)
-    {}
+        , Flags(InFlags)
+    {
+    }
 
     virtual ~FProperty() = default;
+    FProperty(const FProperty&) = default;
+    FProperty& operator=(const FProperty&) = default;
+    FProperty(FProperty&&) = default;
+    FProperty& operator=(FProperty&&) = default;
+
+public:
+    /** ImGui에 각 프로퍼티에 맞는 UI를 띄웁니다. */
+    virtual void DisplayInImGui(UObject* ObjectPtr) const;
+
+    /** 런타임에 타입 정보를 검사하고 업데이트 합니다. */
+    virtual void Resolve(); // TODO: 자식 FProperty 만들면 사용
+
+    /**
+     * 이 프로퍼티가 EPropertyType::Object 타입일 경우, 해당 UObject의 UClass*를 반환합니다.
+     * 그렇지 않거나 아직 해결되지 않은 경우 nullptr을 반환합니다.
+     * @return UClass* 또는 nullptr
+     */
+    FORCEINLINE UClass* GetSpecificClass() const
+    {
+        return GetTypeSpecificDataAs<UClass*>().value_or(nullptr);
+    }
+
+private:
+    /**
+     * TypeSpecificData에서 특정 타입 T의 값을 안전하게 가져옵니다.
+     * 만약 현재 저장된 타입이 T가 아니거나, Type이 해당 T를 가질 수 있는
+     * EPropertyType 상태가 아니라면 std::nullopt를 반환합니다.
+     *
+     * @tparam T 가져오고자 하는 타입 (예: UClass*, FStructInfo*, FName)
+     * @return 해당 타입의 값을 담은 std::optional<T>, 또는 std::nullopt
+     */
+    template <typename T>
+    [[nodiscard]] std::optional<T> GetTypeSpecificDataAs() const
+    {
+        if constexpr (std::same_as<T, UClass*>)
+        {
+            if (Type != EPropertyType::Object)
+            {
+                // Object 타입일 때만 UClass*가 유효
+                return std::nullopt;
+            }
+        }
+        // else if constexpr (std::same_as<T, FStructInfo*>) {
+        //     if (Type != EPropertyType::Struct) { // Struct 타입일 때만 FStructInfo*가 유효
+        //         return std::nullopt;
+        //     }
+        // }
+        else if constexpr (std::same_as<T, FName>)
+        {
+            if (Type != EPropertyType::UnresolvedPointer /* && Type != EPropertyType::UnresolvedStruct 등 */)
+            {
+                return std::nullopt;
+            }
+        }
+
+        // 실제 variant에서 값 가져오기 시도
+        if (std::holds_alternative<T>(TypeSpecificData))
+        {
+            return std::get<T>(TypeSpecificData);
+        }
+        return std::nullopt;
+    }
+
+public:
+    UClass* OwnerClass;
 
     const char* Name;
+    EPropertyType Type;
     int64 Size;
     int64 Offset;
+    EPropertyFlags Flags;
+
+public:
+    std::variant<
+        std::monostate,  // 현재 값이 없음을 나타냄
+        UClass*,         // FProperty의 Type이 Object일때 원본 Property를 가지고 있는 UClass
+        // FStructInfo*,    // FProperty의 Type이 Struct일때 커스텀 구조체의 정보
+        FName            // FProperty의 Type이 UnresolvedPointer일 때 런타임에 검사할 UClass 이름
+    > TypeSpecificData;
 };
 
 
