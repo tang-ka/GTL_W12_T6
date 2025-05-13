75d658150dd4ef529b7b34d8aa77d169a77db27b
gudtldn <gudtldn1234@gmail.com>
2025-05-13 13:47:03 +0900

    feat: TIsTSubclassOf 개념 및 유틸리티 추가

--- EngineSIU/EngineSIU/Engine/Source/Runtime/CoreUObject/Template/SubclassOf.h
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/CoreUObject/Template/SubclassOf.h
@@ -73,3 +73,15 @@ public:
 private:
     UClass* Class = nullptr;
 };
+
+
+template <typename T>
+static constexpr bool TIsTSubclassOf_V = false;
+
+template <typename T> constexpr bool TIsTSubclassOf_V<               TSubclassOf<T>> = true;
+template <typename T> constexpr bool TIsTSubclassOf_V<const          TSubclassOf<T>> = true;
+template <typename T> constexpr bool TIsTSubclassOf_V<      volatile TSubclassOf<T>> = true;
+template <typename T> constexpr bool TIsTSubclassOf_V<const volatile TSubclassOf<T>> = true;
+
+template <typename T>
+concept TIsTSubclassOf = TIsTSubclassOf_V<T>;
