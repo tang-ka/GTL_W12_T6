a458aba9c4771c79f8228a2fb2cb041d48e09698
gudtldn <gudtldn1234@gmail.com>
2025-05-16 19:51:54 +0900

    feat: ScriptStruct 클래스 추가 및 관련 파일 등록

---
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/CoreUObject/UObject/ScriptStruct.cpp
@@ -0,0 +1,12 @@
+﻿#include "ScriptStruct.h"
+
+
+UScriptStruct::UScriptStruct(
+    const char* InName,
+    uint32 InStructSize,
+    uint32 InAlignment,
+    UScriptStruct* InSuperScriptStruct
+)
+    : UStruct(InName, InStructSize, InAlignment, InSuperScriptStruct)
+{
+}
