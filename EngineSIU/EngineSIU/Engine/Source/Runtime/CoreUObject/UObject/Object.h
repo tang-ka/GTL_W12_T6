29072cb79edcf4cce1c16f66714175bc2b6b07d7
gudtldn <gudtldn1234@gmail.com>
2025-05-16 16:52:49 +0900

    refactor: UStruct 클래스에 대한 friend 선언 추가 및 코드 정리

--- EngineSIU/EngineSIU/Engine/Source/Runtime/CoreUObject/UObject/Object.h
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/CoreUObject/UObject/Object.h
@@ -27,7 +27,8 @@ public:
 private:
     friend class FObjectFactory;
     friend class FSceneMgr;
-    friend class UClass;
+    friend class UClass; // TODO: 제거
+    friend class UStruct;
 
     uint32 UUID;
     uint32 InternalIndex; // Index of GUObjectArray
