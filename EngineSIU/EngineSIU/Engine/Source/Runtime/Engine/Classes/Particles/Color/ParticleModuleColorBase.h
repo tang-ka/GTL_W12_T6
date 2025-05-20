770f90130873b7eb6aa93644f2d5e05219616e89
Xerlocked <xhouse.xman@gmail.com>
2025-05-21 03:59:38 +0900

    FIX: ColorBase / ColorOverLife

--- EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Particles/Color/ParticleModuleColorBase.h
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Particles/Color/ParticleModuleColorBase.h
@@ -7,11 +7,16 @@ class UParticleModuleColorBase : public UParticleModule
 {
     DECLARE_CLASS(UParticleModuleColorBase, UParticleModule)
 public:
-    UParticleModuleColorBase() = default;
+    UParticleModuleColorBase();
 
     // Initial Color : When particle is spawn
     UPROPERTY_WITH_FLAGS(EditAnywhere, FDistributionVector, StartColor)
 
     // Initial Alpha : When particle is spawn
     UPROPERTY_WITH_FLAGS(EditAnywhere, FDistributionFloat, StartAlpha)
+
+public:
+    virtual void Spawn(FParticleEmitterInstance* Owner, int32 Offset, float SpawnTime, FBaseParticle* ParticleBase) override;
+
+    virtual void DisplayProperty() override;
 };
--- EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Particles/Color/ParticleModuleColorOverLife.h
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Particles/Color/ParticleModuleColorOverLife.h
@@ -9,7 +9,18 @@ class UParticleModuleColorOverLife : public UParticleModuleColorBase
     DECLARE_CLASS(UParticleModuleColorOverLife, UParticleModuleColorBase)
 
 public:
-    UParticleModuleColorOverLife() = default;
+    UParticleModuleColorOverLife();
     virtual ~UParticleModuleColorOverLife() override = default;
-    
+
+    // Color over life : When particle is update
+    UPROPERTY_WITH_FLAGS(EditAnywhere, FDistributionVector, ColorOverLife)
+
+    // Alpha over life : When particle is update
+    UPROPERTY_WITH_FLAGS(EditAnywhere, FDistributionFloat, AlphaOverLife)
+
+public:
+    virtual void Spawn(FParticleEmitterInstance* Owner, int32 Offset, float SpawnTime, FBaseParticle* ParticleBase) override;
+    virtual void Update(FParticleEmitterInstance* Owner, int32 Offset, float DeltaTime) override;
+
+    virtual void DisplayProperty() override;
 };
