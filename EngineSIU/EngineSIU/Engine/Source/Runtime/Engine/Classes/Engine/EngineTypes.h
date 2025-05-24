5d5fc47593197b861436cdf31543bb99bda0a962
choux-croquant <choifrance@gmail.com>
2025-05-24 15:05:49 +0900

    add bodysetup interface and some physics types

--- EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Engine/EngineTypes.h
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/Engine/EngineTypes.h
@@ -13,3 +13,26 @@ namespace EEndPlayReason
         Quit,
     };
 }
+
+namespace ECollisionEnabled
+{
+    enum Type : int
+    {
+        /** Will not create any representation in the physics engine. Cannot be used for spatial queries (raycasts, sweeps, overlaps) or simulation (rigid body, constraints). Best performance possible (especially for moving objects) */
+        NoCollision,
+        /** Only used for spatial queries (raycasts, sweeps, and overlaps). Cannot be used for simulation (rigid body, constraints). Useful for character movement and things that do not need physical simulation. Performance gains by keeping data out of simulation tree. */
+        QueryOnly ,
+        /** Only used only for physics simulation (rigid body, constraints). Cannot be used for spatial queries (raycasts, sweeps, overlaps). Useful for jiggly bits on characters that do not need per bone detection. Performance gains by keeping data out of query tree */
+        PhysicsOnly,
+        /** Can be used for both spatial queries (raycasts, sweeps, overlaps) and simulation (rigid body, constraints). */
+        QueryAndPhysics,
+        /** Only used for probing the physics simulation (rigid body, constraints). Cannot be used for spatial queries (raycasts,
+        sweeps, overlaps). Useful for when you want to detect potential physics interactions and pass contact data to hit callbacks
+        or contact modification, but don't want to physically react to these contacts. */
+        ProbeOnly,
+        /** Can be used for both spatial queries (raycasts, sweeps, overlaps) and probing the physics simulation (rigid body,
+        constraints). Will not allow for actual physics interaction, but will generate contact data, trigger hit callbacks, and
+        contacts will appear in contact modification. */
+        QueryAndProbe
+    };
+}
---
+++ EngineSIU/EngineSIU/Engine/Source/Runtime/Engine/Classes/PhysicsEngine/AggregateGeom.h
@@ -0,0 +1,246 @@
+#pragma once
+
+#include "UObject/ObjectMacros.h"
+#include "ShapeElem.h"
+
+struct FKAggregateGeom
+{
+    // 각 Geometry Shape에 대한 내용
+    /*UPROPERTY(EditAnywhere, editfixedsize, Category = "Aggregate Geometry", meta = (DisplayName = "Spheres", TitleProperty = "Name"))
+    TArray<FKSphereElem> SphereElems;
+
+    UPROPERTY(EditAnywhere, editfixedsize, Category = "Aggregate Geometry", meta = (DisplayName = "Boxes", TitleProperty = "Name"))
+    TArray<FKBoxElem> BoxElems;
+
+    UPROPERTY(EditAnywhere, editfixedsize, Category = "Aggregate Geometry", meta = (DisplayName = "Capsules", TitleProperty = "Name"))
+    TArray<FKSphylElem> SphylElems;
+
+    UPROPERTY(EditAnywhere, editfixedsize, Category = "Aggregate Geometry", meta = (DisplayName = "Convex Elements", TitleProperty = "Name"))
+    TArray<FKConvexElem> ConvexElems;
+
+    UPROPERTY(EditAnywhere, editfixedsize, Category = "Aggregate Geometry", meta = (DisplayName = "Tapered Capsules", TitleProperty = "Name"))
+    TArray<FKTaperedCapsuleElem> TaperedCapsuleElems;
+
+    UPROPERTY(EditAnywhere, editfixedsize, Category = "Aggregate Geometry", meta = (DisplayName = "Level Sets", TitleProperty = "Name"))
+    TArray<FKLevelSetElem> LevelSetElems;
+
+    UPROPERTY(EditAnywhere, editfixedsize, Category = "Aggregate Geometry", meta = (DisplayName = "(Experimental) Skinned Level Sets", TitleProperty = "Name"), Experimental)
+    TArray<FKSkinnedLevelSetElem> SkinnedLevelSetElems;*/
+
+    FKAggregateGeom()
+        : RenderInfoPtr(nullptr)
+    {
+    }
+
+    FKAggregateGeom(const FKAggregateGeom& Other)
+        : RenderInfoPtr(nullptr)
+    {
+        CloneAgg(Other);
+    }
+
+    const FKAggregateGeom& operator=(const FKAggregateGeom& Other)
+    {
+        FreeRenderInfo();
+        CloneAgg(Other);
+        return *this;
+    }
+
+    // 모든 형상의 개수 합쳐서 던져주기
+    int32 GetElementCount() const
+    {
+        //return SphereElems.Num() + SphylElems.Num() + BoxElems.Num() + ConvexElems.Num() + TaperedCapsuleElems.Num() + LevelSetElems.Num() + SkinnedLevelSetElems.Num();
+    }
+
+    int32 GetElementCount(EAggCollisionShape::Type Type) const;
+
+    FKShapeElem* GetElement(const EAggCollisionShape::Type Type, const int32 Index)
+    {
+        /*switch (Type)
+        {
+        case EAggCollisionShape::Sphere:
+            if (ensure(SphereElems.IsValidIndex(Index))) { return &SphereElems[Index]; }
+        case EAggCollisionShape::Box:
+            if (ensure(BoxElems.IsValidIndex(Index))) { return &BoxElems[Index]; }
+        case EAggCollisionShape::Sphyl:
+            if (ensure(SphylElems.IsValidIndex(Index))) { return &SphylElems[Index]; }
+        case EAggCollisionShape::Convex:
+            if (ensure(ConvexElems.IsValidIndex(Index))) { return &ConvexElems[Index]; }
+        case EAggCollisionShape::TaperedCapsule:
+            if (ensure(TaperedCapsuleElems.IsValidIndex(Index))) { return &TaperedCapsuleElems[Index]; }
+        case EAggCollisionShape::LevelSet:
+            if (ensure(LevelSetElems.IsValidIndex(Index))) { return &LevelSetElems[Index]; }
+        case EAggCollisionShape::SkinnedLevelSet:
+            if (ensure(SkinnedLevelSetElems.IsValidIndex(Index))) { return &SkinnedLevelSetElems[Index]; }
+        default:
+            ensure(false);
+            return nullptr;
+        }*/
+        return nullptr;
+    }
+
+    FKShapeElem* GetElement(const int32 InIndex)
+    {
+        /*int Index = InIndex;
+        if (Index < SphereElems.Num()) { return &SphereElems[Index]; }
+        Index -= SphereElems.Num();
+        if (Index < BoxElems.Num()) { return &BoxElems[Index]; }
+        Index -= BoxElems.Num();
+        if (Index < SphylElems.Num()) { return &SphylElems[Index]; }
+        Index -= SphylElems.Num();
+        if (Index < ConvexElems.Num()) { return &ConvexElems[Index]; }
+        Index -= ConvexElems.Num();
+        if (Index < TaperedCapsuleElems.Num()) { return &TaperedCapsuleElems[Index]; }
+        Index -= TaperedCapsuleElems.Num();
+        if (Index < LevelSetElems.Num()) { return &LevelSetElems[Index]; }
+        Index -= LevelSetElems.Num();
+        if (Index < SkinnedLevelSetElems.Num()) { return &SkinnedLevelSetElems[Index]; }
+        ensure(false);*/
+        return nullptr;
+    }
+
+    const FKShapeElem* GetElement(const int32 InIndex) const
+    {
+        /*int Index = InIndex;
+        if (Index < SphereElems.Num()) { return &SphereElems[Index]; }
+        Index -= SphereElems.Num();
+        if (Index < BoxElems.Num()) { return &BoxElems[Index]; }
+        Index -= BoxElems.Num();
+        if (Index < SphylElems.Num()) { return &SphylElems[Index]; }
+        Index -= SphylElems.Num();
+        if (Index < ConvexElems.Num()) { return &ConvexElems[Index]; }
+        Index -= ConvexElems.Num();
+        if (Index < TaperedCapsuleElems.Num()) { return &TaperedCapsuleElems[Index]; }
+        Index -= TaperedCapsuleElems.Num();
+        if (Index < LevelSetElems.Num()) { return &LevelSetElems[Index]; }
+        Index -= LevelSetElems.Num();
+        if (Index < SkinnedLevelSetElems.Num()) { return &SkinnedLevelSetElems[Index]; }
+        ensure(false);*/
+        return nullptr;
+    }
+
+    const FKShapeElem* GetElementByName(const FName InName) const
+    {
+        /*if (const FKShapeElem* FoundSphereElem = GetElementByName<FKSphereElem>(MakeArrayView(SphereElems), InName))
+        {
+            return FoundSphereElem;
+        }
+        else if (const FKShapeElem* FoundBoxElem = GetElementByName<FKBoxElem>(MakeArrayView(BoxElems), InName))
+        {
+            return FoundBoxElem;
+        }
+        else if (const FKShapeElem* FoundSphylElem = GetElementByName<FKSphylElem>(MakeArrayView(SphylElems), InName))
+        {
+            return FoundSphylElem;
+        }
+        else if (const FKShapeElem* FoundConvexElem = GetElementByName<FKConvexElem>(MakeArrayView(ConvexElems), InName))
+        {
+            return FoundConvexElem;
+        }
+        else if (const FKShapeElem* FoundTaperedCapsuleElem = GetElementByName<FKTaperedCapsuleElem>(MakeArrayView(TaperedCapsuleElems), InName))
+        {
+            return FoundTaperedCapsuleElem;
+        }
+        else if (const FKShapeElem* FoundLevelSetElem = GetElementByName<FKLevelSetElem>(MakeArrayView(LevelSetElems), InName))
+        {
+            return FoundLevelSetElem;
+        }
+        else if (const FKShapeElem* FoundSkinnedLevelSetElem = GetElementByName<FKSkinnedLevelSetElem>(MakeArrayView(SkinnedLevelSetElems), InName))
+        {
+            return FoundSkinnedLevelSetElem;
+        }*/
+
+        return nullptr;
+    }
+
+    int32 GetElementIndexByName(const FName InName) const
+    {
+        /*int32 FoundIndex = GetElementIndexByName<FKSphereElem>(MakeArrayView(SphereElems), InName);
+        int32 StartIndex = 0;
+        if (FoundIndex != INDEX_NONE)
+        {
+            return FoundIndex + StartIndex;
+        }
+        StartIndex += SphereElems.Num();
+
+        FoundIndex = GetElementIndexByName<FKBoxElem>(MakeArrayView(BoxElems), InName);
+        if (FoundIndex != INDEX_NONE)
+        {
+            return FoundIndex + StartIndex;
+        }
+        StartIndex += BoxElems.Num();
+
+        FoundIndex = GetElementIndexByName<FKSphylElem>(MakeArrayView(SphylElems), InName);
+        if (FoundIndex != INDEX_NONE)
+        {
+            return FoundIndex + StartIndex;
+        }
+        StartIndex += SphylElems.Num();
+
+        FoundIndex = GetElementIndexByName<FKConvexElem>(MakeArrayView(ConvexElems), InName);
+        if (FoundIndex != INDEX_NONE)
+        {
+            return FoundIndex + StartIndex;
+        }
+        StartIndex += ConvexElems.Num();
+
+        FoundIndex = GetElementIndexByName<FKTaperedCapsuleElem>(MakeArrayView(TaperedCapsuleElems), InName);
+        if (FoundIndex != INDEX_NONE)
+        {
+            return FoundIndex + StartIndex;
+        }
+        StartIndex += TaperedCapsuleElems.Num();
+
+        FoundIndex = GetElementIndexByName<FKLevelSetElem>(MakeArrayView(LevelSetElems), InName);
+        if (FoundIndex != INDEX_NONE)
+        {
+            return FoundIndex + StartIndex;
+        }
+        StartIndex += LevelSetElems.Num();
+
+        FoundIndex = GetElementIndexByName<FKSkinnedLevelSetElem>(MakeArrayView(SkinnedLevelSetElems), InName);
+        if (FoundIndex != INDEX_NONE)
+        {
+            return FoundIndex + StartIndex;
+        }*/
+
+        return INDEX_NONE;
+    }
+
+    void EmptyElements()
+    {
+        /*BoxElems.Empty();
+        ConvexElems.Empty();
+        SphylElems.Empty();
+        SphereElems.Empty();
+        TaperedCapsuleElems.Empty();
+        LevelSetElems.Empty();
+        SkinnedLevelSetElems.Empty();*/
+
+        FreeRenderInfo();
+    }
+
+    //void GetAggGeom(const FTransform& Transform, const FColor Color, const FMaterialRenderProxy* MatInst, bool bPerHullColor, bool bDrawSolid, bool bOutputVelocity, int32 ViewIndex, class FMeshElementCollector& Collector) const;
+
+    /** Release the RenderInfo (if its there) and safely clean up any resources. Call on the game thread. */
+    void FreeRenderInfo();
+
+    /** Returns the volume of this element */
+    FVector::FReal GetScaledVolume(const FVector& Scale3D) const;
+
+private:
+
+    /** Helper function for safely copying instances */
+    void CloneAgg(const FKAggregateGeom& Other)
+    {
+       /* SphereElems = Other.SphereElems;
+        BoxElems = Other.BoxElems;
+        SphylElems = Other.SphylElems;
+        ConvexElems = Other.ConvexElems;
+        TaperedCapsuleElems = Other.TaperedCapsuleElems;
+        LevelSetElems = Other.LevelSetElems;
+        SkinnedLevelSetElems = Other.SkinnedLevelSetElems;*/
+    }
+
+    mutable std::atomic<class FKConvexGeomRenderInfo*> RenderInfoPtr;
+    //mutable UE::FMutex RenderInfoLock;
+};
